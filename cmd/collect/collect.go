04b3827d9a6194ebafac7466adf198672e5e7071
Juxuny Wu <juxuny@gmail.com>
2017-11-18 17:43:04 +0800

    每隔一分钟自动获取一次

--- cmd/collect/collect.go
+++ cmd/collect/collect.go
@@ -4,33 +4,42 @@ import (
 	stat "github.com/juxuny/statistics"
 	"fmt"
 	"flag"
+	"time"
 )
 
 var dbConfig stat.DBConfig
 
+var debug bool
+
 func init() {
-	stat.SetDebug(true)
+
 	flag.StringVar(&dbConfig.DatabaseName, "name", "stock", "database name")
 	flag.StringVar(&dbConfig.Host, "host", "127.0.0.1", "host address")
 	flag.StringVar(&dbConfig.User, "u", "root", "user for database")
 	flag.StringVar(&dbConfig.Password, "p", "123456", "password")
 	flag.IntVar(&dbConfig.Port, "port", 3306, "port for database")
+	flag.BoolVar(&debug, "d", false, "debug mode")
 	flag.Parse()
+	stat.SetDebug(debug)
 }
 
 func main() {
-	collector, e := stat.NewCollector(stat.STOCK_TYPE, dbConfig)
-	if e != nil {
-		panic(e)
+	for {
+
+		collector, e := stat.NewCollector(stat.STOCK_TYPE, dbConfig)
+		if e != nil {
+			panic(e)
+		}
+		r, e := collector.FetchStockCode()
+		if e != nil {
+			panic(e)
+		}
+		fmt.Print(r)
+		e = collector.Save(r)
+		if e != nil {
+			panic(e)
+		}
+		fmt.Println("save success")
+		time.Sleep(time.Minute * 10)
 	}
-	r, e := collector.FetchStockCode()
-	if e != nil {
-		panic(e)
-	}
-	fmt.Print(r)
-	e = collector.Save(r)
-	if e != nil {
-		panic(e)
-	}
-	fmt.Println("save success")
 }
