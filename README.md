37ecb5ed0365fd2a33e28bada161a1bb9c044786
Juxuny Wu <juxuny@gmail.com>
2018-03-14 18:03:30 +0800

    pre-process:增加时间特征向量

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # statistics
 statistics
 
-### Install
+## 数据收集
+
+#### Install
 ```shell
 go install github.com/juxuny/statistics/cmd/statistics-cli
 ```
 
-### Run
+#### Run
 每天13:00 - 16:00收集数据，保存到MySQL,些前请先把 `res`目录里两个SQL文件导入(`res/index_list.sql`, `res/stock_code.sql`)
 ```shell
 /opt/gopath/bin/collect -u root -p 123456 -d=true -log=/root/2.log -start=13:00 -end=16:00
--- cmd/statistics-cli/pre.go
+++ cmd/statistics-cli/pre.go
@@ -4,6 +4,8 @@ import (
 	stat "github.com/juxuny/statistics"
 	"os"
 	"encoding/csv"
+	"time"
+	"fmt"
 )
 
 var _STANDARDIZATION_HEADER = []string{
@@ -15,11 +17,52 @@ var _STANDARDIZATION_HEADER = []string{
 	"deal", "deal_price",
 	"mean_price", "mean_sell", "mean_buy", "mean_deal_volume", "mean_deal_price",
 	"sd_price", "sd_sell", "sd_buy", "sd_deal_volume", "sd_deal_price",
+	"week_0", "week_1", "week_2", "week_3", "week_4", "week_5", "week_6",
+}
+
+var timeMap map[string]int
+
+func generateTimeMap() (m map[string]int) {
+	m = make(map[string]int)
+	t, e := time.Parse("15:04:05", "09:25:00")
+	if e != nil {
+		log.Panic(e)
+	}
+	ii := 0
+	for t.Format("15:04") <= "16:00" {
+		tmp := t.Format("15:04")
+		m[tmp] = ii
+		ii ++
+		t = t.Add(time.Minute)
+	}
+	return
+}
+
+func initTimeMap() {
+	timeMap = generateTimeMap()
+	var header []string
+	iterator := 0
+	for range timeMap {
+		header = append(header, fmt.Sprintf("t_%d", iterator))
+		iterator ++
+	}
+	_STANDARDIZATION_HEADER = stat.MergeStrings(_STANDARDIZATION_HEADER, header)
+}
+
+func timeToOneHotVector(timeStr string) (oneHot stat.OneHot, e error) {
+	index, b := timeMap[timeStr[:5]]
+	if b {
+		oneHot = stat.NewOneHot(index, len(timeMap))
+		return
+	}
+	e = fmt.Errorf("invalid time")
+	return
 }
 
 
 //数据导出预处理
 func pre() {
+	initTimeMap()
 	if !stat.CheckDate(start) || !stat.CheckDate(end) {
 		log.Print("invalid date")
 		return
@@ -60,8 +103,19 @@ func pre() {
 		w := csv.NewWriter(f)
 		w.Write(_STANDARDIZATION_HEADER[2:])
 		for _, v := range dataStandardization {
+			t, e := time.Parse("2006-01-02 15:04:05", v.Date + " " + v.Time)
+			if e != nil {
+				log.Println(e)
+				continue
+			}
+			weekOneHot := stat.NewOneHot(int(t.Weekday()), 7)
+			timeOneHot, e := timeToOneHotVector(t.Format("15:04"))
+			if e != nil {
+				log.Println(e)
+				continue
+			}
 			row := v.ToStrings()[2:]
-			row = stat.MergeStrings(row, mean.ToStrings(), sd.ToStrings())
+			row = stat.MergeStrings(row, mean.ToStrings(), sd.ToStrings(), weekOneHot.ToStrings(), timeOneHot.ToStrings())
 			e = w.Write(row)
 			if e != nil {
 				log.Println(e)
